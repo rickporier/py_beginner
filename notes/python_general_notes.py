2d22518dd34fab4b9dc8900956777437248f8a0a
Rick Porier <rick@rickporier.com>
2025-09-18 05:02:31 -0500

    Add gneral notes example

--- notes/python_general_notes.py
+++ notes/python_general_notes.py
@@ -15,3 +15,45 @@ my_set= {1,2,3}
 
 my_tuple=(1,2,3)
 
+# Dictionaries - Contained in curly braces. 
+
+my_dict={'key': 'value'}
+
+#Dictionairy Example
+
+my_dict={
+    'name':'raj',
+    'role': 'coder',
+    'salary': '120000'
+}
+# Left column are KEYS (each must be unique). The right are values
+#   and do not have to be unique. 
+
+my_dict['age']=30  #this will add the KEY 'age' and the VALUE '30'
+
+print(my_dict.get('age'))
+
+print(my_dict.keys()) # This is called a 'View Object'.
+        # Looks like a list when printed but not a list.
+
+# all the things you can use as a VALUE in a dictionairy.
+    # - Strings, intergers, floats, lists, booleans, tuple, 
+        # a set, a dictionary (inside the dictionairy).
+
+#EXAMPLE: Name as the KEY and a dictionairy as a VALUE.
+
+my_emp={
+    'Raj':{
+        'role':'coder',
+        'salary':'120000'
+    },
+    'Sam':{
+        'role':'designer',
+        'salary':'115000'
+
+    }
+}
+
+my_emp['Raj']['salary']=135000 # Raj got a raise.
+
+print(my_emp.get('Raj'))
